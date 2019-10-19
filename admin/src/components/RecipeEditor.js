0a054282ed1a7bd196efe53b6aeee304665bc806
Austin Hughes <austindavidhughes@gmail.com>
2019-10-18 22:12:24 -0600

    Recipe editor UI done, need to hook up to API now.

--- admin/src/components/RecipeEditor.js
+++ admin/src/components/RecipeEditor.js
@@ -18,7 +18,8 @@ function RecipeEditor({ recipe, isOpen, onClose }) {
 			ingredients: [],
 			steps: []
 		};
-		setCurrentRecipe(Object.assign(blankRecipe, recipe));
+		const copy = JSON.parse(JSON.stringify(Object.assign(blankRecipe, recipe)));
+		setCurrentRecipe(copy);
 	}, [recipe]);
 
 	const updateTitle = e => setCurrentRecipe({ ...currentRecipe, title: e.target.value })
@@ -27,24 +28,102 @@ function RecipeEditor({ recipe, isOpen, onClose }) {
 
 	const { ingredients, steps } = currentRecipe;
 
+	const editIngredientAmount = (e, i) => {
+		const { ingredients } = currentRecipe;
+		ingredients[i].amount = e.target.value;
+		setCurrentRecipe({ ...currentRecipe, ingredients });
+	}
+
+	const editIngredientName = (e, i) => {
+		const { ingredients } = currentRecipe;
+		ingredients[i].name = e.target.value;
+		setCurrentRecipe({ ...currentRecipe, ingredients });
+	}
+
+	const newIngredient = () => {
+		const { ingredients } = currentRecipe;
+		ingredients.push({ amount: '', name: '' });
+		setCurrentRecipe({ ...currentRecipe, ingredients });
+	}
+
+	const removeIngredient = i => {
+		const { ingredients } = currentRecipe;
+		ingredients.splice(i, 1);
+		setCurrentRecipe({ ...currentRecipe, ingredients });
+	}
+
+	const moveIngredientUp = i => {
+		const { ingredients: ing } = currentRecipe;
+		[ing[i - 1], ing[i]] = [ing[i], ing[i - 1]]
+		setCurrentRecipe({ ...currentRecipe, ingredients: ing });
+	}
+
+	const moveIngredientDown = i => {
+		const { ingredients: ing } = currentRecipe;
+		[ing[i + 1], ing[i]] = [ing[i], ing[i + 1]]
+		setCurrentRecipe({ ...currentRecipe, ingredients: ing });
+	}
+
 	const ingredientRows = ingredients.map((ingredient, i) => (
 		<div key={i}>
 			<div className="amt">
-				<input type="text" value={ingredient.amount} onChange={() => {}}></input>
+				<input type="text" value={ingredient.amount} onChange={e => editIngredientAmount(e, i)}></input>
 			</div>
 			<div className="ingredient">
-				<input type="text" value={ingredient.name} onChange={() => {}}></input>
+				<input type="text" value={ingredient.name} onChange={e => editIngredientName(e, i)}></input>
+			</div>
+			<div className="edit-buttons">
+				<button className="remove" onClick={() => removeIngredient(i)}>X</button>
+				<button className="up-down" onClick={() => moveIngredientUp(i)} disabled={i === 0}>&#8593;</button>
+				<button className="up-down" onClick={() => moveIngredientDown(i)} disabled={i === ingredients.length - 1}>&#8595;</button>
 			</div>
 		</div>
 	));
+	ingredientRows.push(<button key="add" className="add" onClick={newIngredient}>+</button>);
 	const ingredientsText = ingredients.map(i => [i.amount, i.name].join(' ')).join('\n');
 
 
+	const editStep = (e, i) => {
+		const steps = [...currentRecipe.steps];
+		steps[i].description = e.target.value;
+		setCurrentRecipe({ ...currentRecipe, steps });
+	}
+
+	const newStep = () => {
+		const { steps } = currentRecipe;
+		steps.push({ description: '' });
+		setCurrentRecipe({ ...currentRecipe, steps });
+	}
+
+	const removeStep = i => {
+		const { steps } = currentRecipe;
+		steps.splice(i, 1);
+		setCurrentRecipe({ ...currentRecipe, steps });
+	}
+
+	const moveStepUp = i => {
+		const { steps: s } = currentRecipe;
+		[s[i - 1], s[i]] = [s[i], s[i - 1]]
+		setCurrentRecipe({ ...currentRecipe, steps: s });
+	}
+
+	const moveStepDown = i => {
+		const { steps: s } = currentRecipe;
+		[s[i + 1], s[i]] = [s[i], s[i + 1]]
+		setCurrentRecipe({ ...currentRecipe, steps: s });
+	}
+
 	const stepsRows = steps.map((s, i) => (
-		<div key={i}>
-			<input type="text" value={s.description} onChange={() => {}}></input>
+		<div className="step-edit" key={i}>
+			<textarea value={s.description} onChange={e => editStep(e, i)}></textarea>
+			<div className="edit-buttons">
+				<button className="remove" onClick={() => removeStep(i)}>X</button>
+				<button className="up-down" onClick={() => moveStepUp(i)} disabled={i === 0}>&#8593;</button>
+				<button className="up-down" onClick={() => moveStepDown(i)} disabled={i === steps.length - 1}>&#8595;</button>
+			</div>
 		</div>
 	));
+	stepsRows.push(<button key="add" className="add" onClick={newStep}>+</button>);
 	const stepsText = steps.map((s, i) => (
 		<div key={i} className="step">{`${i + 1}. ${s.description}`}</div>
 	));
@@ -94,7 +173,7 @@ function RecipeEditor({ recipe, isOpen, onClose }) {
 						</label>
 					</div>
 					<div className="ingredients" onClick={e => e.stopPropagation()}>
-						<div className="header">
+						<div className="header" onClick={cancelEdit}>
 							Ingredients
 						</div>
 						{ isIngredientEdit ?
@@ -103,7 +182,7 @@ function RecipeEditor({ recipe, isOpen, onClose }) {
 						}
 					</div>
 					<div className="steps" onClick={e => e.stopPropagation()}>
-						<div className="header">
+						<div className="header" onClick={cancelEdit}>
 							Steps
 						</div>
 						{ isStepsEdit ?
